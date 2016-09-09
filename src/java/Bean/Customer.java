583b91373f9b91a97400cf0165040a758e8247b3
Vricky <949927007@qq.com>
2016-09-09 09:47:23 +0800

    Customer类

---
+++ src/java/Bean/Customer.java
@@ -0,0 +1,32 @@
+package Bean;
+
+public class Customer {
+    private final String id,firstName,lastName;
+    private final double balance;
+
+    public Customer(String id, String firstName, String lastName, double balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+    
+    
+}
